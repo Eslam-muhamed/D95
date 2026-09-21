cc9b5b8aec50260857b2910bfc79ffd56e1ae76c
Eslam-muhamed <jrsllam@gmail.com>
2026-09-21 17:59:05 +0300

    chore: remove obsolete test and scratch files

--- test-rls.sql
+++
@@ -1,3 +0,0 @@
-insert into public.tournament_participants (tournament_id, player_name, phone, status, payment_method)
-values ('11111111-1111-1111-1111-111111111111', 'المستخدم 1', '01012345678', 'pending', 'cash')
-returning *;
